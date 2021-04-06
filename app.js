feaafa29dad8a38bcdf8da4000a6840262b877b4
Shreyas12345678 <shreyaskamathbelle27@gmail.com>
2021-04-06 11:15:51 +0530

    chabged app.js and index

--- app.js
+++ app.js
@@ -5,7 +5,20 @@
 
  .controller('Mycontroller', function($scope){
 
-    $scope.name = "shreyas kamath";
+    $scope.name ="";
+    $scope.totalValue=0;
+
+    $scope.display = function() {
+    	var total=calculate($scope.name);
+    	$scope.totalValue=total;
+    };
+    function calculate(string){
+    	var v=0;
+    	for(var i=0;i<string.length;i++){
+    		v+=string.charCodeAt(i);
+    	}
+    	return v;
+    }
 
  });
 
